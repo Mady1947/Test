a042dd9df89a2323fc750c3ea49427621a6aa8d8
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-18 00:47:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,14 +10,6 @@ variable "region" {
   type        = string
   default     = ""
 }
-variable "ami" {
-  type        = string
-  ami ="ami-03dbf9550d4620230"
-}
-variable "instancetype" {
-  type        = string
-  instancetype ="t2.micro"
-}
 variable "vm_names" {
   type        = list(string)
   default     = ["node1","node2","node3"]
