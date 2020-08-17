05f0239ebbd8e008be397d3ff1067d0738a3465a
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-18 00:34:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,78 +1,22 @@
-variable aws_region {
-  description = "AWS region"
+variable "accessKey" {
   type        = string
-  default     = "us-east-1"
+  default     = ""
 }
-
-variable project {
-  description = "Map of project names to configuration"
-  type        = map
-  default     = {
-    project-alpha = {
-      public_subnet_count  = 2,
-      private_subnet_count = 2,
-      instances_per_subnet = 2,
-      instance_type        = "t2.micro",
-      environment          = "dev"
-    },
-    project-beta = {
-      public_subnet_count  = 1,
-      private_subnet_count = 1,
-      instances_per_subnet = 2,
-      instance_type        = "t2.nano",
-      environment          = "test"
-    }
-  }
-}
-
-variable vpc_cidr_block {
-  description = "CIDR block for VPC"
+variable "secretKey" {
   type        = string
-  default     = "10.0.0.0/16"
+  default     = ""
 }
-
-variable public_subnet_cidr_blocks {
-  description = "Available cidr blocks for public subnets"
-  type        = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24",
-    "10.0.7.0/24",
-    "10.0.8.0/24",
-    "10.0.9.0/24",
-    "10.0.10.0/24",
-    "10.0.11.0/24",
-    "10.0.12.0/24",
-    "10.0.13.0/24",
-    "10.0.14.0/24",
-    "10.0.15.0/24",
-    "10.0.16.0/24"
-  ]
+variable "region" {
+  type        = string
+  default     = ""
 }
-
-variable private_subnet_cidr_blocks {
-  description = "Available cidr blocks for private subnets"
+variable "ami" {
+  type        = string
+}
+variable "instancetype" {
+  type        = string
+}
+variable "vm_names" {
   type        = list(string)
-  default = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    "10.0.104.0/24",
-    "10.0.105.0/24",
-    "10.0.106.0/24",
-    "10.0.107.0/24",
-    "10.0.108.0/24",
-    "10.0.109.0/24",
-    "10.0.110.0/24",
-    "10.0.111.0/24",
-    "10.0.112.0/24",
-    "10.0.113.0/24",
-    "10.0.114.0/24",
-    "10.0.115.0/24",
-    "10.0.116.0/24"
-  ]
+  default     = ["node1","node2","node3"]
 }
